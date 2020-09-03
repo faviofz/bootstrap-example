60de5aef544707bfd177a9c888133baaa7170eab
Fz <favioo.wow@gmail.com>
2020-09-03 05:32:39 -0300

    Terminado Grid de Categorias

--- js/script.js
+++ js/script.js
@@ -8,36 +8,41 @@ $(document).ready(function () {
 		speed: 500,
 		autoplay: true,
 		autoplayButtonOutput: false,
-		touch: false
+		touch: false,
+		autoHeight: true
 	});
 
-	var alturaBanner = $(".my-slider div.img").height();
-	console.log(alturaBanner);
+	var alturaBanner = $(".my-slider").height();
 	$(".bannerEstatico").css({ height: alturaBanner + "px" });
+
+	$(window).scroll(function () {
+		var posY = window.pageYOffset;
+
+		if (posY > alturaBanner) {
+			$("header").css("background-color", "white");
+			$("header .logotipo").css("filter", "invert(100%)");
+			$(".fa-search, .fa-bars").css("color", "black");
+		} else {
+			$("header").css("background-color", "rgba(0,0,0,0.5)");
+			$("header .logotipo").css("filter", "invert(0%)");
+			$(".fa-search, .fa-bars").css("color", "white");
+		}
+	});
+	/* ------------------------------- ANCHOR MENU ------------------------------ */
+	$(".fa-bars").click(function () {
+		$(".menu").fadeIn("fast");
+	});
+	$(".btnClose").click(function () {
+		$(".menu").fadeOut("fast");
+	});
+
+	/* -------------------------- ANCHOR GRID CATEGORIA ------------------------- */
+
+	$(".grid figure").mouseover(function () {
+		$(this).css({ "background-position": "right bottom" });
+	});
+
+	$(".grid figure").mouseout(function () {
+		$(this).css({ "background-position": "left top" });
+	});
 });
-// var alturaBanner = $("div.bx-viewport").style.height;
-// console.log(alturaBanner);
-// var alturaBanner = $(".slick-list").height();
-// $(".bannerEstatico").css({ height: alturaBanner + "px" });
-
-// $(window).scroll(function () {
-// 	var posY = window.pageYOffset;
-
-// 	if (posY > alturaBanner) {
-// 		// $("header").css("background-color", "white");
-// 		$("header .logotipo").css("filter", "invert(100%)");
-// 		$(".fa-search, .fa-bars").css("color", "black");
-// 	} else {
-// 		$("header").css("background-color", "rgba(0,0,0,0.5)");
-// 		$("header .logotipo").css("filter", "invert(0%)");
-// 		$(".fa-search, .fa-bars").css("color", "white");
-// 	}
-// });
-
-// /* ------------------------------- ANCHOR MENU ------------------------------ */
-// $(".fa-bars").click(function () {
-// 	$(".menu").fadeIn("fast");
-// });
-// $(".btnClose").click(function () {
-// 	$(".menu").fadeOut("fast");
-// });
